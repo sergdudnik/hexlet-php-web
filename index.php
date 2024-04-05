23b9dd9f6399025a24252b41f47e6ff2ec953a6c
Sergei Dudnik (on Mac) <bigbulk@gmail.com>
2024-04-05 18:03:09 +0300

    changes in index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<h1>hello, php!</h1>
 <?php
 
 // Печатает год в STDOUT
@@ -5,6 +6,7 @@ echo date('d-M-Y');
 echo "\n";
 echo 'Hello, world!';
 print_r("\nOK");
+echo '<h1>i am from PHP!</h1>';
 // echo json_encode($_SERVER);
 // var_dump($_SERVER);
 echo json_encode($_GET);
