26958e1bdc7268a115605354590bf8949e3fe6f1
Sergei Dudnik (on Mac) <bigbulk@gmail.com>
2024-04-05 19:04:00 +0300

    change index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-<h1>hello, php!</h1>
 <?php
-
-// Печатает год в STDOUT
-echo date('d-M-Y');
-echo "\n";
-echo 'Hello, world!';
-print_r("\nOK");
-echo '<h1>i am from PHP!</h1>';
-// echo json_encode($_SERVER);
-// var_dump($_SERVER);
-echo json_encode($_GET);
+$links = [
+    ['url' => 'https://google.com', 'name' => 'Google'],
+    ['url' => 'https://yandex.com', 'name' => 'Yandex'],
+    ['url' => 'https://bingo.com', 'name' => 'Bingo']
+];
+?>
+    <?php foreach ($links as $link) : ?>
+        <div><a href="<?= $link['url'] ?>"><?= $link['name'] ?></a></div>
+    <?php endforeach; ?>
