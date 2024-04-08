79028d1119d4e99eb3f2b45ed6699f2fb7ab770a
Sergei Dudnik (on Mac) <bigbulk@gmail.com>
2024-04-08 22:27:02 +0300

    changes in index.php

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php
+
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$faker = \Faker\Factory::create();
+$faker->seed(1234);
+
+$domains = [];
+for ($i = 0; $i < 10; $i++) {
+    $domains[] = $faker->domainName;
+}
+
+$phones = [];
+for ($i = 0; $i < 10; $i++) {
+    $phones[] = $faker->phoneNumber;
+}
+
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('go to the /phones or /domains');
+    return $response;
+});
+
+// BEGIN (write your solution here)
+$app->get('/phones', function (Request $request, Response $response) use ($phones) {
+    echo(json_encode($phones));
+    return $response
+                ->withHeader('Content-Type', 'application/json');
+});
+$app->get('/domains', function (Request $request, Response $response) use ($domains) {
+    $response->getBody()->write(json_encode($domains));
+    return $response
+                ->withHeader('Content-Type', 'application/json');
+});
+// END
+
+$app->run();
